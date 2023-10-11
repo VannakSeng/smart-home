e0b7065ca38daef332a81bd6aaca4a32c8845ea9
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2023-10-11 01:22:13 +0000

    Updating to latest FlutterFlow output.

--- lib/flutter_flow/nav/nav.dart
+++ lib/flutter_flow/nav/nav.dart
@@ -296,14 +296,13 @@ class FFRoute {
                 )
               : builder(context, ffParams);
           final child = appStateNotifier.loading
-              ? Center(
-                  child: SizedBox(
-                    width: 50.0,
-                    height: 50.0,
-                    child: CircularProgressIndicator(
-                      valueColor: AlwaysStoppedAnimation<Color>(
-                        FlutterFlowTheme.of(context).primary,
-                      ),
+              ? Container(
+                  color: FlutterFlowTheme.of(context).primary,
+                  child: Center(
+                    child: Image.asset(
+                      'assets/images/smart-house.png',
+                      width: MediaQuery.sizeOf(context).width * 0.5,
+                      fit: BoxFit.contain,
                     ),
                   ),
                 )
